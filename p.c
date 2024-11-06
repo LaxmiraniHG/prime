3b0e276fe9a30309e03f604b96573071e4c6ccb4
prakruthigs <prakruthigs82@gmail.com>
2024-11-06 16:55:51 +0530

    Update p.c

--- p.c
+++ p.c
@@ -1,1 +1,25 @@
-int first = 0, second = 1, next; printf("Fibonacci Series: "); for (int i = 0; i < n; i++) { if (i == 0) { printf("%d ", first); } else if (i == 1) { printf("%d ", second); } else { next = first + second; first = second; second = next; printf("%d ", next); } } printf("\n"); } int isPrime(int num) { if (num <= 1) return 0; // 0 and 1 are not prime for (int i = 2; i <= sqrt(num); i++) { if (num % i == 0) { return 0; // Not prime } } return 1; // Prime } long long powerOfXtoX(int x) { return pow(x, x); }
+int first = 0, second = 1, next; printf("Fibonacci Series: ");
+for (int i = 0; i < n; i++)
+  { 
+    if (i == 0)
+    { printf("%d ", first);
+    } 
+    else if (i == 1) 
+    {
+      printf("%d ", second); 
+    }
+    else
+    { next = first + second; first = second; second = next;
+     printf("%d ", next);
+       } }
+printf("\n"); 
+}
+int isPrime(int num)
+{ if (num <= 1) return 0;
+ for (int i = 2; i <= sqrt(num); i++)
+{ if (num % i == 0) { return 0;
+                    } }
+ return 1;
+} long long powerOfXtoX(int x) 
+{ return pow(x, x); 
+}
